9f0cad303d5837e5bfe7b6e6a6b74b734741b8b5
opsullivan85 <opsullivan85@gmail.com>
2020-12-06 12:46:36 -0800

    Working on file structure

--- lasagna/__main__.py
+++ lasagna/__main__.py
@@ -1,14 +1,15 @@
 import numpy as np
 import trimesh
 from trimesh.base import Trimesh
+from . import testpy
 
 if __name__ == '__main__':
     # attach to logger so trimesh messages will be printed to console
-    trimesh.util.attach_to_log()
+    # trimesh.util.attach_to_log()
     mesh: Trimesh
-    mesh = trimesh.load('../Cube.STL')
+    mesh = trimesh.load('Cube.stl')
     print('here')
     #print(mesh.face_adjacency)
     print(mesh.edges_sorted)
-    #mesh.show()
+    mesh.show()
     print('done')
