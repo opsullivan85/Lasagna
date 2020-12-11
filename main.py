0e42bb81d8f475e2b4ddea863e8ec4c2da01715b
opsullivan85 <opsullivan85@gmail.com>
2020-12-11 00:07:10 -0500

    get triangles in slice improved algorithm

--- main.py
+++ main.py
@@ -7,17 +7,29 @@ from time import time
 
 
 def get_section(tmesh: Trimesh, height: float):
-    t0 = time()
     # (face, side, vert, xyz)
     mesh = np.asarray(tmesh.vertices)[tmesh.edges_unique][tmesh.faces_unique_edges]
 
-    # List of z coords for each face
-    vertices = np.reshape(mesh[:, :, :, 2], (-1, 6))
-    vertices = vertices > height
+    # Are vertices of face above height
+    vertices = np.greater(np.reshape(mesh[:, :, :, 2], (-1, 6)), height)
 
     # perform xor on each row
     intersection_mask = np.logical_and(np.logical_not(np.all(vertices, 1)), np.any(vertices, 1))
-    print(time() - t0)
+
+
+def get_intersecting_faces(tmesh: Trimesh, height: float):
+
+    # are vertices above  height
+    vertices = np.greater(tmesh.vertices[:, 2], height)
+
+    # each vertex appears twice here. Find better implementation. slowest part right now
+    # are the verticies in each face above height
+    array_vertices = np.reshape(vertices[tmesh.edges_unique][tmesh.faces_unique_edges], (-1, 6))
+
+    intersection_mask = np.logical_and(np.logical_not(np.all(array_vertices, 1)), np.any(array_vertices, 1))
+
+    return intersection_mask
+
 
 
 if __name__ == '__main__':
@@ -26,4 +38,16 @@ if __name__ == '__main__':
 
     mesh: Trimesh
     mesh = trimesh.load('bunny.stl')
+    # = trimesh.load('Cube.stl')
+    t = time()
+    get_intersecting_faces(mesh, 1)
+    print(time() - t)
+
+    t = time()
     get_section(mesh, 1)
+    print(time() - t)
+
+    t = time()
+    get_intersecting_faces(mesh, 1)
+    print(time() - t)
+
