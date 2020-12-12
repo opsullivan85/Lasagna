d477ffd87e858da17c77148f0191ab75a1a52529
opsullivan85 <opsullivan85@gmail.com>
2020-12-11 20:03:14 -0500

    Intersecting faces optimized

--- main.py
+++ main.py
@@ -6,17 +6,24 @@ from trimesh.base import Trimesh
 from time import time
 
 
-def get_intersecting_faces(tmesh: Trimesh, height: float):
-    # are vertices above  height
-    vertices = np.greater(tmesh.vertices[:, 2], height)
+def get_intersecting_faces_lst(tmesh: Trimesh, height: np.ndarray):
+    if not type(height) == np.ndarray:
+        if not hasattr(height, '__iter__'):
+            height = np.asarray([height])
+        else:
+            height = np.asarray(height)
+
+    # are vertices above height
+    vertices = np.greater(np.repeat(tmesh.vertices[:, 2, np.newaxis], height.shape[0], axis=1), height)
 
     # each vertex appears twice here, but keeping them is faster than the overhead required to remove them
-    # are the verticies in each face above height
-    array_vertices = np.reshape(vertices[tmesh.edges_unique][tmesh.faces_unique_edges], (-1, 6))
+    face_vertices = np.reshape(vertices[tmesh.edges_unique][tmesh.faces_unique_edges], (-1, 6, height.shape[0]))
 
-    intersection_mask = np.logical_and(np.logical_not(np.all(array_vertices, 1)), np.any(array_vertices, 1))
+    # does face have vertices above and below a slice
+    intersection_mask = np.logical_and(np.logical_not(np.all(face_vertices, 1)), np.any(face_vertices, 1))
 
-    intersections = np.where(intersection_mask)
+    # get this to work?
+    intersections = np.nonzero(intersection_mask)
 
     return intersection_mask
 
@@ -26,18 +33,11 @@ if __name__ == '__main__':
     # trimesh.util.attach_to_log()
 
     mesh: Trimesh
-    #mesh = trimesh.load('bunny.stl')
-    mesh = trimesh.load('Cube.stl')
-    print(get_intersecting_faces(mesh, 1))
-    num = 1000
-    num2 = 1
+    mesh = trimesh.load('bunny.stl')
+    # mesh = trimesh.load('Cube.stl')
+    slices = np.linspace(mesh.bounds[0, 2], mesh.bounds[1, 2], 1000)
+    get_intersecting_faces_lst(mesh, 0)
 
     t = time()
-    for i in range(num):
-        intersections = get_intersecting_faces(mesh, 1)
-        for j in range(num2):
-            mesh.faces[intersections]
+    get_intersecting_faces_lst(mesh, slices)
     print(time() - t)
-
-
-
